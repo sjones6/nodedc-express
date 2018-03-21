2554d19a648b68dc65f2acb4a851761529747113
sjones6 <jones.spencera@gmail.com>
2018-03-21 07:11:04 -0400

    Add code from presentation

--- tests/users-test.js
+++ tests/users-test.js
@@ -1,7 +1,14 @@
 const assert = require("assert")
 
+const request = require("supertest")
+const app = require("app")
+
 describe("Users", () => {
-  it("should do something", () => {
-    assert.equal(true, true)
+  it("should return a resource", done => {
+    request(app)
+      .get('/users')
+      .expect(200)
+      .then(() => done())
+      .catch(done)
   })
 })
